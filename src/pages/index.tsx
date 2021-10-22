fc56776ea0a88b3cd5229f363d173f50100e1a96
Natália Fonseca <nataliabf@outlook.com>
2021-10-21 21:36:44 -0300

    chore: initialize project

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Magister | Atividades Extracurriculares</title>
+        <meta
+          name="description"
+          content="Módulo de gerênciamento de atividades extracurriculares do Magister."
+        />
+      </Head>
+
+      <h1>Magister | Atividades Extracurriculares</h1>
+    </div>
+  );
+};
+
+export default Home;
