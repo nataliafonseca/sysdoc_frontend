710002fc5362d721a9231baf7c4f6f04f108a4da
Natália Fonseca <nataliabf@outlook.com>
2021-10-25 01:04:42 -0300

    feat: turn header logo into link to index

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ import {
   useColorMode,
   useColorModeValue
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import { ColorModeToggle } from '../ColorModeToggle';
 import { Profile } from './Profile';
 
@@ -30,12 +31,22 @@ export function Header() {
       align="center"
       bg={useColorModeValue('gray.100', 'gray.700')}
     >
-      <Text fontSize={['2xl', '4xl']} fontFamily="quicksand" textAlign="center">
-        magister
-        <Text as="span" color={colorMode === 'light' ? 'blue.500' : 'blue.200'}>
-          doc
+      <Link href="/" passHref>
+        <Text
+          as="a"
+          fontSize={['2xl', '4xl']}
+          fontFamily="quicksand"
+          textAlign="center"
+        >
+          magister
+          <Text
+            as="span"
+            color={colorMode === 'light' ? 'blue.500' : 'blue.200'}
+          >
+            doc
+          </Text>
         </Text>
-      </Text>
+      </Link>
 
       <Flex align="center" ml="auto">
         <ColorModeToggle
